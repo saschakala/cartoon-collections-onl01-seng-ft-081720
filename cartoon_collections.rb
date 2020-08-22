459d099bf52772cd3048b8a750a3d1c8cff882b6
“saschakala” <mxsaschakala@gmail.com>
2020-08-22 12:53:45 -0600

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(name)
+  name.each.with_index(1) do |name, i| puts "#{i}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  veggies.map do |name| "#{name.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |value| value.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+def find_the_cheese (cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |type| cheese_types.include? (type)
+  end
 end
